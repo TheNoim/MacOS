2750e79d0bffb1a849f87db4b16569f88e07fdcd
Nils Bergmann <nilsbergmann@noim.io>
2020-08-22 20:38:48 +0200

    Update

---
+++ global-shell.zsh
@@ -0,0 +1,20 @@
+
+# Git commit shortcut
+gcommit() {
+	if [ -z "$@" ]; then
+		git commit -a -S
+	else
+		git commit -a -S -m $@
+	fi
+}
+
+# Find out which app is listening on a port
+listening() {
+    if [ $# -eq 0 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P
+    elif [ $# -eq 1 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color $1
+    else
+        echo "Usage: listening [pattern]"
+    fi
+}
