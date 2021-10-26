baa2db5027646d0bac08d4fa452ebd30f02030f6
Nils Bergmann <nilsbergmann@noim.io>
2021-10-26 14:59:50 +0200

    Add gitlab-test alias

--- global-shell.zsh
+++ global-shell.zsh
@@ -19,4 +19,14 @@ listening() {
     fi
 }
 
+gitlab-test() {
+	gitlab-runner exec docker \
+		--docker-volumes /var/run/docker.sock:/var/run/docker.sock \
+		--docker-volumes ~/.docker/gitlab.json:/root/.docker/config.json:ro \
+		--docker-privileged \
+		--env REGISTRY=registry.schuelerkarriere.de \
+		--env DOCKER_AUTH_CONFIG="$(cat ~/.docker/gitlab.json)" \
+		$@
+}
+
 export HOMEBREW_NO_AUTO_UPDATE=1
