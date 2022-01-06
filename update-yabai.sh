f5389b9a4f1e91c67086b4c12ab1509df2f0eb18
Nils Bergmann <nilsbergmann@noim.io>
2022-01-06 15:06:50 +0100

    Update yabai update script

--- update-yabai.sh
+++ update-yabai.sh
@@ -2,10 +2,13 @@
 
 brew services stop yabai
 
-brew reinstall yabai.rb
+brew uninstall yabai.rb
+
+brew install yabai.rb
 
 sudo yabai --uninstall-sa
-
 sudo yabai --install-sa
 
 brew services start yabai
+
+sudo yabai --load-sa
