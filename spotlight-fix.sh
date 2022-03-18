ef8deda2efd8e94305d0b3142f92b6dc0924aa69
Nils Bergmann <nilsbergmann@noim.io>
2022-03-18 13:33:21 +0100

    Add spotlight fix script

---
+++ spotlight-fix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ -z $(which spotlight-manager) ]]; then
+	if [[ -z $(which npm) ]];
+		echo "You need to install nodejs with npm first"
+		exit 1
+	fi
+	npm install -g spotlight-manager
+fi
+
+# Add all node_modules to blacklist
+sudo spotlight-manager exclude node_modules ~
+
+# Make sure spotlight is enabled
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+
+# Rebuild index
+sudo mdutil -E /
